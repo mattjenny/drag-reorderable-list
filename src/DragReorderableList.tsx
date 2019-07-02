22bc02904cd08c0c10c267b33088f7fe192676df
Matt Jenny <mattjenny92@gmail.com>
2019-07-02 15:08:31 -0500

    Checkpoint

--- src/DragReorderableList.tsx
+++ src/DragReorderableList.tsx
@@ -7,14 +7,103 @@ const ListWrapper = styled.ol`
     padding-inline-start: 0;
 `;
 
-export class DragReorderableList extends React.PureComponent<{}, {}> {
+interface TestData {
+    title: string;
+}
+
+interface ChildElement {
+    id: string;
+    height: number;
+    data: TestData;
+}
+
+interface State {
+    draggingItem: DragData | undefined;
+    childElements: Array<ChildElement>;
+}
+
+const testData = [
+    { id: "t1", title: "Test 1" },
+    { id: "t2", title: "Test 2" },
+    { id: "t3", title: "Test 3" },
+]
+
+interface DragData {
+    id: string;
+    mouseOffset: number;
+}
+
+export class DragReorderableList extends React.PureComponent<{}, State> {
+    public state = {
+        draggingItem: undefined,
+        childElements: testData.map((item) => ({
+            id: item.id,
+            height: 100,
+            data: { title: item.title },
+        })),
+    }
+
+    public componentDidMount() {
+        window.addEventListener('mouseup', this.handleMouseUp);
+    }
+
+    public componentWillUnmount() {
+        window.removeEventListener('mouseup', this.handleMouseUp);
+    }
+
     public render() {
         return (
             <ListWrapper>
-                <DragReorderableListItem title="Test 1" displayIndex={1} />
-                <DragReorderableListItem title="Test 2" displayIndex={2} />
-                <DragReorderableListItem title="Test 3" displayIndex={3} />
+                {this.state.childElements.map((item, idx) => (
+                    <DragReorderableListItem
+                        key={item.id}
+                        id={item.id}
+                        title={item.data.title}
+                        displayIndex={idx + 1}
+                        height={item.height}
+                        onDrag={this.handleDragStart}
+                        onResize={this.childResized}
+                    />
+                ))}
             </ListWrapper>
         )
+    }
+
+    private handleDragStart = (id: string, e: React.MouseEvent<HTMLLIElement>) => {
+        const childElement = this.state.childElements.find((item) => item.id === id);
+        if (childElement) {
+            this.setState({
+                draggingItem: {
+                    id,
+                    mouseOffset: 5,
+                },
+            });
+
+            window.addEventListener('mousemove', this.handleMouseMove);
+        }
+    }
+
+    private handleMouseMove = (e: any) => {
+        console.log('Mousemove: ', e);
+    }
+
+    private handleMouseUp = () => {
+        if (this.state.draggingItem) {
+            // Clean up
+            this.setState({ draggingItem: undefined });
+            window.removeEventListener('mousemove', this.handleMouseMove);
+        }
+    }
+
+    private childResized = (id: string, height: number) => {
+        const childElementIdx = this.state.childElements.findIndex((item) => item.id === id);
+        if (childElementIdx) {
+            const updatedChildElements = [ ...this.state.childElements ];
+            updatedChildElements[childElementIdx] = {
+                ...updatedChildElements[childElementIdx],
+                height,
+            };
+            this.setState({ childElements: updatedChildElements });
+        }
     }
 }
--- src/DragReorderableListItem.tsx
+++ src/DragReorderableListItem.tsx
@@ -9,16 +9,47 @@ const ListItem = styled.li`
 export interface ListItemDisplayProps {
     title: string;
     displayIndex: number;
+    id: string;
+    height: number;
+    onDrag: (id: string, e: React.MouseEvent<HTMLLIElement>) => void;
+    onResize: (id: string, height: number) => void;
 }
 
 export interface OwnProps extends ListItemDisplayProps {}
 
-export class DragReorderableListItem extends React.PureComponent<OwnProps, {}> {
+interface State {
+    isSelected: boolean;
+    isDragging: boolean;
+}
+
+export class DragReorderableListItem extends React.PureComponent<OwnProps, State> {
+    private el: HTMLLIElement | null | undefined;
+    public state = {
+        isSelected: false,
+        isDragging: false,
+    };
+
+    public componentDidMount() {
+        if (this.el) {
+            this.props.onResize(this.props.id, this.el.getBoundingClientRect().height);
+        }
+    }
+
     public render() {
         return (
-            <ListItem>
+            <ListItem ref={el => this.el = el} onMouseDown={this.handleMouseDown}>
                 <DemoListItemComponent {...this.props} />
             </ListItem>
         )
+    }
+
+    private handleMouseDown = (e: React.MouseEvent<HTMLLIElement>) => {
+        this.props.onDrag(this.props.id, e);
+    }
+
+    private handleMouseUp = () => {
+        if (this.state.isDragging) {
+            this.setState({ isDragging: false });
+        }
     }
 }
